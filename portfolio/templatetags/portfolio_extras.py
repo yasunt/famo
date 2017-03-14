45ca3e0890f94ae832b30abd938a97edee8e9a46
yasunt <kistat.info@gmail.com>
2017-03-14 18:59:02 +0900

    mypage prototype created.

--- portfolio/templatetags/portfolio_extras.py
+++ portfolio/templatetags/portfolio_extras.py
@@ -2,10 +2,21 @@ from django import template
 
 register = template.Library()
 
-from counsel.models import Question
+from counsel.models import Answer, Question
 
 
 @register.filter
 def get_questions(user):
     questions = Question.objects.filter(user=user)[:10]
     return questions
+
+def latest_user_objs(model_, user, num):
+    return model_.objects.filter(user=user)[:num]
+
+@register.filter
+def latest_answers(user, num):
+    return latest_user_objs(Answer, user, num)
+
+@register.filter
+def latest_questions(user, num):
+    return latest_user_objs(Question, user, num)
