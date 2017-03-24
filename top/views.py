2f1c8ecab46e79278969cc789f82a8d352f152b5
yasunt <kistat.info@gmail.com>
2017-03-24 20:32:33 +0900

    version 0.02

--- top/views.py
+++ top/views.py
@@ -5,14 +5,18 @@ from articles.models import Article
 from counsel.models import Question
 from accounts.forms import RegistrationForm
 from django.conf import settings
-
+from datetime import datetime
 
 def index(request):
     categories = [{'name': '育児・子育て', 'img_url': 'commons/images/baby.jpg', 'key': 'child'}, {'name': '教育・習い事', 'img_url': 'commons/images/study.jpg', 'key': 'study'},
         {'name': '妊娠・出産', 'img_url': 'commons/images/pregnant.jpg', 'key': 'pregnant'}, {'name': '結婚・夫婦生活', 'img_url': 'commons/images/couple02.jpg', 'key': 'couple'}
     ]
     keywords = ['引きこもり', '介護', '保育園', '病気', '主夫', '家事', '高齢出産']
-    context = {'popular_article': Article.objects.order_by('-hits')[0], 'popular_question': Question.objects.order_by('-hits')[0], 'categories': categories, 'keywords': keywords}
+    today_questions = Question.objects.filter(updated_date__date=datetime.today())
+    popular_question = today_questions.order_by('-hits')[0] if today_questions.count() > 0 else Question.objects.order_by('-hits')[0]
+    today_articles = Article.objects.filter(registered_date__date=datetime.today())
+    popular_article = today_articles.order_by('-hits')[0] if today_articles.count() > 0 else Article.objects.order_by('-hits')[0]
+    context = {'popular_article': popular_article, 'popular_question': popular_question, 'categories': categories, 'keywords': keywords}
     return render(request, 'top/index.html', context)
 
 def signup(request):
